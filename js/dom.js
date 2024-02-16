bd64c7af3104920b1e119dfd07ed045415b389d2
sadiaprova12 <sadiaprova51717@gmail.com>
2024-02-16 21:29:15 +0600

    document

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,6 @@
-console.log('Hello From JS')
+console.log('Hello From JS');
+console.log(document.body);
+
+// DOM - Document object Model
+// Dom 3 types : (1) HTML [mark up], (2) CSS [style], and (3) JavaScript [Manipulate] ek sathy mile DOM hoy.
+// DOM Tree
